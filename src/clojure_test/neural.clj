48ab8e99760d91f8fafd1d6985dce2ec653033d4
glebcher601 <gleb.cherepukhin@nixsolutions.com>
2018-02-14 19:01:31 +0200

    Working on simple perceptron #1

--- src/clojure_test/neural.clj
+++ src/clojure_test/neural.clj
@@ -18,25 +18,86 @@
      (recur (conj list value) value (- number 1))
      list)))
 
+(defn paired-reduce
+  [first-seq second-seq result apply-fx]
+  (if (or (empty? first-seq) (empty? second-seq))
+    result
+    (let [x 1]
+      (println result)
+      (recur (rest first-seq) (rest second-seq) (apply-fx (first first-seq) (first second-seq) result) apply-fx)
+      )
+    ))
+
 (defn generate-pairs-of2vecs
-  "Taskes pairs of two vector and makes vector of pairs of two corresponding elements,
-  vectors must be equal length, otherwise only full pairs will be included"
-  ([vector-first vector-last]
-    (generate-pairs-of2vecs vector-first vector-last []))
-  ([vector-first vector-last result]
-   (if (or (empty? vector-first) (empty? vector-last))
-     result
-     (recur (rest vector-first) (rest vector-last) (concat (vector (vector (first vector-first) (first vector-last))) result))
-     )))
+  [vector-first vector-last]
+  (paired-reduce vector-first vector-last []
+                 (fn [elem-first elem-second result]
+                   (into result (vector (vector elem-first elem-second))))))
+
+(defn change-weights
+  [inputs weights change-fx]
+  (let [new-weights (paired-reduce inputs weights '()
+                                   (fn [elem-first elem-second result]
+                                     (conj result (if (= elem-first 1) (change-fx elem-second) elem-second))))]
+    (reverse new-weights)))
+
+(defn decrease-weights
+  [inputs weights]
+  (change-weights inputs weights #(- % 1)))
+
+(defn increase-weights
+  [inputs weights]
+  (change-weights inputs weights #(+ % 1)))
 
 (defn proceed
   [inputs weights bias]
-  (let [result (reduce +  (map #(* (first %) (last %)) (generate-pairs-of2vecs inputs weights)))]
+  (let [result (reduce + (map #(* (first %) (last %)) (generate-pairs-of2vecs inputs weights)))]
     (> result bias)))
 
-(defn decrease-weights-if-errors
-  "If our network commited error, decrease energized inputs' weight"
-  ([input-weights-pairs]
-    (map #((if (= % 1))) input-weights-pairs)
+(defn change-weights-after-classify
+  [generated-number inputs weights]
+  (if (not (= generated-number 5))
+    (if (proceed inputs weights 7)
+      (decrease-weights inputs weights)
+      weights)
+    (if (not (proceed inputs weights 7))
+      (increase-weights inputs weights)
+      weights)
+    )
+  )
+
+(defn learn-neural
+  ([learn-iterations]
+   (let [train-data
+         (prepareData cseq->intseq
+                      "111101101101111"
+                      "001001001001001"
+                      "111001111100111"
+                      "111001111001111"
+                      "101101111001001"
+                      "111100111001111"
+                      "111100111101111"
+                      "111001001001001"
+                      "111101111101111"
+                      "111101111001111")
+         test-data
+         (prepareData cseq->intseq
+                      "111100111000111"
+                      "111100010001111"
+                      "111100011001111"
+                      "110100111001111"
+                      "110100111001011"
+                      "111100101001111"
+                      )
+         weights
+         (generate-weights 0 15)]
+     ))
+  ([train-data weights learn-iterations]
+   (if (= learn-iterations 0)
+     weights
+     (let [generated-number (rand-int 10)]
+       (recur train-data (change-weights-after-classify))
+       )
+     )
     )
   )
